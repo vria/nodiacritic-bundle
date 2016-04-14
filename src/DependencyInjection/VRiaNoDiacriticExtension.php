82a8b9725a319d51c49068051613e70f1081b156
Vlad Ria <riabchenko.vlad@gmail.com>
2016-04-14 16:03:29 +0200

    service

--- src/DependencyInjection/VRiaNoDiacriticExtension.php
+++ src/DependencyInjection/VRiaNoDiacriticExtension.php
@@ -2,8 +2,10 @@
 
 namespace VRia\Bundle\NoDiacriticBundle\DependencyInjection;
 
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
 
 class VRiaNoDiacriticExtension extends Extension
 {
@@ -12,6 +14,7 @@ class VRiaNoDiacriticExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
     }
 }
